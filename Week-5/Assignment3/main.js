4e88b64dbb6df4ca9d98b3c69a72988553cb0210
Parisha <parisha.workshop@gmail.com>
2022-12-21 16:07:38 +0530

    Updated Ass#3 with edge cases solution

--- Week-5/Assignment3/main.js
+++ Week-5/Assignment3/main.js
@@ -7,17 +7,34 @@
 
 // so if the set of the array and the array itself have the same length then there are no duplicates
 
-hasDuplicate = MyArray =>  {
-    const mySet = new Set(MyArray);
-    return mySet.size !== MyArray.length ? true : false ;
+hasDuplicate = arr => {
+    //addressing edge cases where input array has empty, null or undefined elements
+    if (arr.includes(undefined) || arr.includes(null) || arr.includes("")) {
+        return "the array has null, undefined or empty arguments";
+    }
+    else {
+        const mySet = new Set(arr);
+        return mySet.size !== arr.length ? true : false;
+    }
 }
-
 console.log(hasDuplicate([1,5,-1,-5])); //false
 console.log(hasDuplicate([1,3,-1,3])); //true
 console.log(hasDuplicate([2,6,5,5])); //true
+console.log(hasDuplicate([null,undefined,null,5])); //the array has null, undefined or empty arguments
+console.log(hasDuplicate([ ,1,7,5])); //the array has null, undefined or empty arguments
 
 /* 
     Approach 2: 
-    const hasDuplicate = arr => new Set(arr).size !== arr.length
+   
+const hasDuplicate = arr => 
+{ 
+    if (arr.includes(undefined) || arr.includes(null) || arr.includes(""))
+    {
+        return "the array has null, undefined or empty arguments";
+    }
+    else {
+        return new Set(arr).size !== arr.length;
+    } 
+}
 
 */
