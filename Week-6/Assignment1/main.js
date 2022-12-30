025b302a5c14a4f7ec084a53c42b763d9e2407e5
Parisha <parisha.workshop@gmail.com>
2022-12-30 13:27:22 +0530

    Updated with meanungful variable name, conditions for edge cases for empty, null or undefined inputs

--- Week-6/Assignment1/main.js
+++ Week-6/Assignment1/main.js
@@ -1,12 +1,18 @@
-const maxSubArray = function (A) {
-    //initializing the maxumum sum to be negative of infinity as it will be eventually higher than that if the array has all intergers
+const maxSubArray = function (arr) {
+    if ( arr?.includes(undefined) || arr?.includes(null) || arr?.includes(" ")|| arr==null || arr==undefined){
+        if(!Array.isArray(arr)) console.log("input is not an array");
+        else console.log("the array has null, undefined or empty arguments");
+        return [];
+    } 
+    else {
+        //initializing the maxumum sum to be negative of infinity as it will be eventually higher than that if the array has all intergers
     let maxSumSoFar= -Infinity;
     //initializing the sum counter to be 0
     let sum = 0;
     //running a loop through the array to add the sum of each number to the counter
-    for (let i =0;i<A.length;i++) {
+    for (let i =0;i<arr.length;i++) {
         //adding the integer at current index to the sum counter
-        sum+= A[i];
+        sum+= arr[i];
         //checking for max value and assignning the larger of the two values to the maxSumSoFar to be returned as result
         maxSumSoFar= Math.max(sum, maxSumSoFar);
         //if the starting number is negative or the sum changes to negative we need to reset the sum value to calculate the possible max sum that comes next
@@ -14,13 +20,17 @@ const maxSubArray = function (A) {
             sum=0;
         }
     }
-
     return maxSumSoFar;
+    } 
 };
-//TC: O(N)
-//SC: O(C)
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 //output:6
 console.log(maxSubArray([1, 2, 3, 4, -10]));
 //output:10 
+console.log(maxSubArray([5,4,-1,7,8]));
+//output:23 
+console.log(maxSubArray([undefined]));
+//output:1 the array has null, undefined or empty arguments []
+console.log(maxSubArray(null));
+//output:input is not an array [] 
