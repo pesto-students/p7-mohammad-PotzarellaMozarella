07b881c1099aa60ccf11f18ba2af92d2a729786c
Parisha <parisha.workshop@gmail.com>
2023-02-06 10:12:07 +0530

    Created files and imported dependancies- bootstrap and fontawesome included

---
+++ Week-9/to-do-list/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck, faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
+
+function App() {
+  
+}
+
+export default App;
