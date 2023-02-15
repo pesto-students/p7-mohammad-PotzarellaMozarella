ac6fd32d81d54f677952da7f301336b8cdba499b
Parisha <parisha.workshop@gmail.com>
2023-02-15 17:35:18 +0530

    Added footer

--- Week-10/urlshortener/src/Components/PgFooter.jsx
+++ Week-10/urlshortener/src/Components/PgFooter.jsx
@@ -1,45 +1,38 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faCircleCheck,
-  faPen,
-  faTrashCan,
-  faBackspace,
-  faFaceSmileBeam,
-} from "@fortawesome/free-solid-svg-icons";
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function PgFooter() {
   return (
     <>
       <footer 
       className="bg-black text-center text-white border-top">
-        <div className="container p-">
-          <section className="mb-4 d-flex p-2 justify-content-center">
-            <div className="pt-4">
-              <span href="/////" className="p-4 icon">
-                <FontAwesomeIcon icon={faCircleCheck} />
-              </span>
-              <span href="/////" className="p-4 icon">
-                <FontAwesomeIcon icon={faCircleCheck} />
-              </span>
-              <span href="/////" className="p-4 icon" >
-                <FontAwesomeIcon icon={faCircleCheck} />
-              </span>
-              <span href="/////" className="p-4 icon">
-                <FontAwesomeIcon icon={faCircleCheck} />
-              </span>
-              <span href="/////" className="p-4 icon">
-                <FontAwesomeIcon icon={faCircleCheck} />
-              </span>
-              <span href="/////" className="p-4 icon">
-                <FontAwesomeIcon icon={faCircleCheck} />
-              </span>
+        {/* social icons */}
+        <div className="container">
+          <section className="mb-4 d-flex p-4 justify-content-center">
+            <div className="icons pt-4">
+              <a href="/////" className="btn btn-outline-light rounded-circle btn-floating m-3">
+                <FontAwesomeIcon icon={['fab', 'twitter']} />
+              </a>
+              <a href="/////" className="btn btn-outline-light btn-floating rounded-circle m-3">
+                <FontAwesomeIcon icon={['fab', 'facebook']} />
+              </a>
+              <a href="/////" className="btn btn-outline-light btn-floating rounded-circle m-3" >
+                <FontAwesomeIcon icon={['fab', 'linkedin']} />
+              </a>
+              <a href="/////" className="btn btn-outline-light btn-floating rounded-circle m-3">
+                <FontAwesomeIcon icon={['fab', 'github']} />
+              </a>
+              <a href="/////" className="btn btn-outline-light btn-floating rounded-circle m-3">
+                <FontAwesomeIcon icon={['fab', 'instagram']} />
+              </a>
+              <a href="/////" className="btn btn-outline-light btn-floating rounded-circle m-3">
+                <FontAwesomeIcon icon={['fab', 'google']} />
+              </a>
             </div>
           </section>
         </div>
-
+        {/* footer node with copyright info */}
         <div className="container p-4">
           <a href=""
           className="text-white text-decoration-none">
