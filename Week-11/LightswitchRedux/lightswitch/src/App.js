4df0e6b1d56975be44848979211e0cf45e92e427
Parisha <parisha.workshop@gmail.com>
2023-02-23 12:54:31 +0530

    Styling applied

--- Week-11/LightswitchRedux/lightswitch/src/App.js
+++ Week-11/LightswitchRedux/lightswitch/src/App.js
@@ -5,19 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   const [toggle, setToggle] = useState(true);
-    const lightedness = toggle ? "dark" : "lit";
-    const handleFlip = () => {
-        setToggle(!toggle)
-        }
+  const lightedness = toggle ? "dark" : "lit";
+  const handleFlip = () => {
+    setToggle(!toggle)
+  }
   return (
     <div className="App">
-      <>
-                <div className={toggle ? 'bg-dark text-light h-100' : 'bg-light text-dark h-100'}>
-                    <p>the room is {lightedness}</p>
-                    <br />
-                    <button onClick={handleFlip}>flip</button>
-                </div>
-            </>
+      
+      <div className='container mt-4'>
+      <div className={toggle ? 'bg-dark text-light min-vh-100 pt-4' : 'bg-light text-dark min-vh-100 pt-4'}>
+          <p>the room is {lightedness}</p>
+          <br />
+          <button className='mt-10' onClick={handleFlip}>flip</button>
+        </div>
+      </div>
+        
+      
     </div>
   );
 }
