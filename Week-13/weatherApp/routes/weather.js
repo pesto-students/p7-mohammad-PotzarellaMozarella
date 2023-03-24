1b991e996d6c629e227e183b5c38694232178729
Parisha <parisha.workshop@gmail.com>
2023-03-24 18:15:31 +0530

    Router added

---
+++ Week-13/weatherApp/routes/weather.js
@@ -0,0 +1,47 @@
+//importing express library
+const express = require('express')
+
+//the router looks exactly like our app and has the 
+//same methods- get, put, etc
+const router = express.Router()
+
+//api key for weather app
+const apiKey = "256455341c0747b5ba181106232403"
+
+// Setup your default display on launch
+router.get("/", function (req, res) {
+    // It will not fetch and display any data in the index page
+    res.render("index", { cityWeather: null, error: null });
+});
+
+router.post('/', (req, res) => {
+    let city = req.body.city;
+    var request = require('request')
+    request(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`,
+        function (error, response, body) {
+            if (error) {
+                res.render('index', { error: 'Pls try again', cityWeather: null })
+            }
+            else {
+                let data = JSON.parse(body);
+                console.log(data)
+                let cityWeather = {
+                    name: data.location.name,
+                    country: data.location.country,
+                    condition: data.current.condition.text,
+                    temp: data.current.temp_c,
+                    humidity: data.current.humidity
+                }
+                let headline = "The current weather condition"
+                res.render("index", {
+                    cityWeather: cityWeather,
+                    headline: headline,
+                    error: null
+                });
+            }
+        }
+    );
+})
+
+
+module.exports = router;
